9121c24f175df6c2c5dae6243e42fdc241c261a6
Guillermo Fernández <guiferviz@gmail.com>
2015-12-07 13:56:59 +0100

    Fixing tests.

--- tests/services_tests.py
+++ tests/services_tests.py
@@ -7,21 +7,25 @@ from service import ParserService
 class ServicesTests(test_suite.AppEngineTestBase):
 
     def test_at_hour(self):
+        chat_id = 0
         text = "/set at 20:30 Dinner time."
-        ok = ParserService.parse_command(text)
+        ok = ParserService.parse_command(chat_id, text)
         self.assertTrue(ok)
 
     def test_at_hour_one_figure(self):
+        chat_id = 0
         text = "/set at 1:30 Sleep time."
-        ok = ParserService.parse_command(text)
+        ok = ParserService.parse_command(chat_id, text)
         self.assertTrue(ok)
 
     def test_at_minute(self):
+        chat_id = 0
         text = "/set at 20:30 Dinner time."
-        ok = ParserService.parse_command(text)
+        ok = ParserService.parse_command(chat_id, text)
         self.assertTrue(ok)
 
     def test_at_time_error(self):
+        chat_id = 0
         text = "/set at 340:12230 Future time."
-        ok = ParserService.parse_command(text)
+        ok = ParserService.parse_command(chat_id, text)
         self.assertFalse(ok)
